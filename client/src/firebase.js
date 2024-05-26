54d93975d5b1cf97bcf9f79af4edc5a26d92dd51
Advait Bhitkar <bhitkaradvait@gmail.com>
2024-05-26 23:47:51 +0530

    initial commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8FVgLP2QKt6AmPmnN3Ri1WcCyuTWmW00",
+  authDomain: "rento-c8f8a.firebaseapp.com",
+  projectId: "rento-c8f8a",
+  storageBucket: "rento-c8f8a.appspot.com",
+  messagingSenderId: "293564418398",
+  appId: "1:293564418398:web:90cd5629d930fc1e3527cf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
